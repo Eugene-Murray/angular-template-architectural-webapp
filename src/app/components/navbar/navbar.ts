68f6ab06fd23407fb67b3f2d26838c9f90526e1c
Eugene Murray <eugene.murray@gmail.com>
2025-06-11 11:44:16 +0100

    Add toggle menu

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss'
+  styleUrl: './navbar.scss',
 })
-export class Navbar {
+export class Navbar implements AfterViewInit {
+  toggleMenu: boolean = false;
+  // @ViewChild('navbar') navbar: ElementRef | undefined;
+  // @ViewChild('menuIcon') menuIcon: ElementRef | undefined;
 
+  ngAfterViewInit() {
+    // if (this.navbar && this.menuIcon) {
+    //   this.navbar.nativeElement.style.setProperty('--menu-icon-width', `${this.menuIcon.nativeElement.offsetWidth}px`);
+    // }
+  }
+
+  onToggleMenu() {
+    this.toggleMenu = !this.toggleMenu;
+  }
 }
