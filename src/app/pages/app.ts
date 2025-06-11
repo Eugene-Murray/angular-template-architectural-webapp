4580bad3c3eab9ddc99e815f801428d60153d946
Eugene Murray <eugene.murray@gmail.com>
2025-06-11 11:30:55 +0100

    add componets

--- src/app/pages/app.ts
+++ src/app/pages/app.ts
@@ -4,10 +4,13 @@ import { Navbar } from "../components/navbar/navbar";
 import { LandingSection } from "../components/landing-section/landing-section";
 import { About } from "../components/about/about";
 import { Projects } from "../components/projects/projects";
+import { Customers } from "../components/customers/customers";
+import { Contact } from "../components/contact/contact";
+import { Footer } from "../components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, LandingSection, About, Projects],
+  imports: [RouterOutlet, Navbar, LandingSection, About, Projects, Customers, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
